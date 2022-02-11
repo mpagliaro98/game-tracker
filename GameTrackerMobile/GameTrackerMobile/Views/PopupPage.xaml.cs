b147931b1edc26cd23f5624a15084ea64b50d44a
Michael Pagliaro <mpagliaro98@yahoo.com>
2022-02-10 20:28:13 -0500

    Added basic popup window and implemented delete on games

---
+++ GameTrackerMobile/GameTrackerMobile/Views/PopupPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using GameTrackerMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GameTrackerMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopupPage : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        private TaskCompletionSource<Tuple<PopupViewModel.EnumOutputType, string>> _taskCompletionSource;
+        public Task<Tuple<PopupViewModel.EnumOutputType, string>> PopupClosedTask => _taskCompletionSource.Task;
+
+
+        public PopupPage(string title, string message, PopupViewModel.EnumInputType inputType)
+        {
+            InitializeComponent();
+            BindingContext = new PopupViewModel(title, message, inputType);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _taskCompletionSource = new TaskCompletionSource<Tuple<PopupViewModel.EnumOutputType, string>>();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _taskCompletionSource.SetResult(((PopupViewModel)BindingContext).ReturnValue);
+        }
+    }
+}
---
+++ GameTrackerMobile/GameTrackerMobile/Views/PopupPageType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameTrackerMobile.Views
+{
+    public enum PopupPageType
+    {
+        YES_NO
+    }
+}
