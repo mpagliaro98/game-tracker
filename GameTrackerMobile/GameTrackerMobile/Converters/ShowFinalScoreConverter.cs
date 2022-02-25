4f27c03645c0b42aa4bf8d004d392a594bc972cc
Michael Pagliaro <mpagliaro98@yahoo.com>
2022-02-25 18:16:20 -0500

    Score only shows if the status is marked as finished

---
+++ GameTrackerMobile/GameTrackerMobile/Converters/ShowFinalScoreConverter.cs
@@ -0,0 +1,28 @@
+﻿using GameTracker.Model;
+using GameTrackerMobile.Services;
+using RatableTracker.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GameTrackerMobile.Converters
+{
+    public class ShowFinalScoreConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ObjectReference key = (ObjectReference)value;
+            RatingModuleGame rm = ModuleService.GetActiveModule();
+            CompletionStatus status = rm.FindStatus(key);
+            if (status == null) return false;
+            return status.UseAsFinished;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
