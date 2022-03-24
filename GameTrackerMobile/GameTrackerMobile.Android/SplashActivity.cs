ff317bb6b52c3d0df1d92b044ffa06c032fae851
Michael Pagliaro <mpagliaro98@yahoo.com>
2022-03-23 22:38:33 -0400

    Added a splash screen to android

---
+++ GameTrackerMobile/GameTrackerMobile.Android/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTrackerMobile.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startup = new Task(() => { StartActivity(new Intent(Application.Context, typeof(MainActivity))); });
+            startup.Start();
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
