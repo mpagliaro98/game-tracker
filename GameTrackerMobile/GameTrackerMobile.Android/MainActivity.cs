6bd647a8c68879d8e5333a86376a5413ddd759af
Michael Pagliaro <mpagliaro98@yahoo.com>
2022-03-23 22:00:00 -0400

    Fixed home screen app name on android

--- GameTrackerMobile/GameTrackerMobile.Android/MainActivity.cs
+++ GameTrackerMobile/GameTrackerMobile.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace GameTrackerMobile.Droid
 {
-    [Activity(Label = "GameTrackerMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Game Tracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
