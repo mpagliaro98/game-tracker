f59cefdbf1856ca19bdb35f394fca9f95a6fa930
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-02-02 17:12:52 -0500

    New MAUI project

---
+++ GameTrackerMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GameTrackerMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
