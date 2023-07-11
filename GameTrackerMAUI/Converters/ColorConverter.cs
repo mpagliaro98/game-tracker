bc9bcfafc153df24de133da6582f5f8ce730b895
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-07-11 17:53:36 -0400

    Fix for status and platform colors not showing on the game detail page

---
+++ GameTrackerMAUI/Converters/ColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTrackerMAUI.Converters
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((RatableTracker.Util.Color)value).ToMAUIColor();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
