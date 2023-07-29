d1056fc5c4bc254792ef2d189a27feea6d53dabb
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-07-28 22:59:24 -0400

    Final publish changes

--- GameTrackerWPF/Properties/AssemblyInfo.cs
+++ GameTrackerWPF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameTrackerWPF")]
+[assembly: AssemblyTitle("Game Tracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameTrackerWPF")]
+[assembly: AssemblyProduct("Game Tracker")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
