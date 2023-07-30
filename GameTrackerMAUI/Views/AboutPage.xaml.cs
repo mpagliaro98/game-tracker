42aff8a5e0e7655932ae1727d79185bd2d8623e3
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-07-30 13:26:09 -0400

    Buttons to go to the project page

--- GameTrackerMAUI/Views/AboutPage.xaml.cs
+++ GameTrackerMAUI/Views/AboutPage.xaml.cs
@@ -12,4 +12,30 @@ public partial class AboutPage : ContentPage
                 "\nGitHub: github.com/mpagliaro98" +
                 "\nThis open-source software is covered under the MIT license, see the license in the GitHub repository for more information.";
     }
+
+    private async void ButtonRepo_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Uri uri = new Uri("https://github.com/mpagliaro98/game-tracker");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        catch
+        {
+            // An unexpected error occurred. No browser may be installed on the device.
+        }
+    }
+
+    private async void ButtonUpdates_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Uri uri = new Uri("https://github.com/mpagliaro98/game-tracker/releases");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        catch
+        {
+            // An unexpected error occurred. No browser may be installed on the device.
+        }
+    }
 }
