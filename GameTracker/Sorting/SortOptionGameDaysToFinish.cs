b20eaeb46c1e1e56b175b1ad8535a6512499136f
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-07-30 14:00:51 -0400

    New sort option for days to finish

---
+++ GameTracker/Sorting/SortOptionGameDaysToFinish.cs
@@ -0,0 +1,25 @@
+﻿using RatableTracker.ListManipulation.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTracker.Sorting
+{
+    [SortOption(typeof(GameObject))]
+    public class SortOptionGameDaysToFinish : SortOptionSimpleBase<GameObject>
+    {
+        public override string Name => "Days to Finish";
+
+        public SortOptionGameDaysToFinish() : base() { }
+
+        protected override object GetSortValue(GameObject obj)
+        {
+            if (obj.IsUnfinishable)
+                return 0;
+            else
+                return obj.StartedOn > DateTime.MinValue && obj.FinishedOn > DateTime.MinValue ? (obj.FinishedOn - obj.StartedOn).Days : 0;
+        }
+    }
+}
