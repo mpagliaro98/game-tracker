3542920c0996ce5f53131e4abebfb23bb8238c8b
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-08-05 11:31:40 -0400

    New filter options for games

---
+++ GameTracker/Filtering/FilterOptionGameFinished.cs
@@ -0,0 +1,21 @@
+﻿using RatableTracker.ListManipulation.Filtering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTracker.Filtering
+{
+    [FilterOption(typeof(GameObject))]
+    public class FilterOptionGameFinished : FilterOptionBooleanBase<GameObject>
+    {
+        public override string Name => "Finished";
+        public override string DisplayText => "Games that have been finished";
+
+        public override Func<GameObject, bool> GenerateFilterExpression()
+        {
+            return (obj) => obj.IsFinished;
+        }
+    }
+}
---
+++ RatableTracker/ListManipulation/Filtering/FilterOptionModelShowScore.cs
@@ -0,0 +1,21 @@
+﻿using RatableTracker.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatableTracker.ListManipulation.Filtering
+{
+    [FilterOption(typeof(IModelObjectStatus))]
+    public class FilterOptionModelShowScore : FilterOptionBooleanBase<IModelObjectStatus>
+    {
+        public override string Name => "Showing Score";
+        public override string DisplayText => "Items that have a visible score";
+
+        public override Func<IModelObjectStatus, bool> GenerateFilterExpression()
+        {
+            return (obj) => obj.ShowScore;
+        }
+    }
+}
