e6459114eda899878ab455b7e0fb3118d2272ab4
Michael Pagliaro <mpagliaro98@yahoo.com>
2023-12-10 11:14:31 -0500

    Indicator of games not owned

---
+++ GameTrackerMAUI/Converters/NotOwnedFontAttributesConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTrackerMAUI.Converters;
+
+public class NotOwnedFontAttributesConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null) return FontAttributes.None;
+        bool val = (bool)value;
+        return val ? FontAttributes.Italic : FontAttributes.None;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GameTrackerMAUI/Converters/NotOwnedOpacityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTrackerMAUI.Converters;
+
+public class NotOwnedOpacityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null) return 1.0;
+        bool val = (bool)value;
+        return val ? 0.7 : 1.0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
