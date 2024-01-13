02685bf69b344056940c2285fae4ab834039d851
Michael Pagliaro <mpagliaro98@yahoo.com>
2024-01-13 16:55:24 -0500

    Filter option for score ranges

---
+++ RatableTracker/ListManipulation/Filtering/FilterOptionModelRange.cs
@@ -0,0 +1,24 @@
+﻿using RatableTracker.Model;
+using RatableTracker.Modules;
+using RatableTracker.ScoreRanges;
+using RatableTracker.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RatableTracker.ListManipulation.Filtering;
+
+[FilterOption(typeof(RatedObject))]
+public class FilterOptionModelRange : FilterOptionListBase<RatedObject>
+{
+    public override string Name => "Score Range";
+    public override List<KeyValuePair<UniqueID, string>> ListValues => new List<KeyValuePair<UniqueID, string>>()
+        {
+            new KeyValuePair<UniqueID, string>(UniqueID.BlankID(), "Does not fit a Score Range")
+        }.Concat(((TrackerModuleScores)Module).GetScoreRangeList().OrderBy(p => p.Name.CleanForSorting()).Select(p => new KeyValuePair<UniqueID, string>(p.UniqueID, p.Name))).ToList();
+
+    public override Func<RatedObject, bool> GenerateFilterExpression()
+    {
+        return (obj) => FilterID.HasValue() ? obj.ScoreRangeDisplay != null && obj.ScoreRangeDisplay.UniqueID.Equals(FilterID) : obj.ScoreRangeDisplay == null;
+    }
+}
